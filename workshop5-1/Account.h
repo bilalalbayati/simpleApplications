2a140483485de2fb2b5d0466ca37d411872b6cc8
Bilal Albayati <96085406+bilalalbayati@users.noreply.github.com>
2022-01-08 10:26:44 -0500

    Add files via upload

---
+++ workshop5-1/Account.h
@@ -0,0 +1,33 @@
+
+#ifndef SDDS_ACCOUNT_H_
+#define SDDS_ACCOUNT_H_
+#include <iostream>
+
+namespace sdds
+{
+    class Account
+    {
+        int m_number;
+        double m_balance;
+        void setEmpty();
+    public:
+        Account();
+        Account(int number, double balance);
+        std::ostream& display()const;
+        operator bool() const;
+        operator int() const;
+        operator double() const;
+        bool operator ~() const;
+        Account &operator =(int number);
+        Account &operator =(Account & accA);
+        Account &operator +=(double balance);
+        Account &operator -=(double balance);
+        Account &operator <<(Account& accA);
+        Account &operator >>(Account& accA);
+        friend double operator +(const Account &accA, const Account &accB);
+        friend double operator +=(double & balance, const Account& accA);
+    };
+
+
+}
+#endif // SDDS_ACCOUNT_H_
